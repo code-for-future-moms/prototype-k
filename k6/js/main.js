327da8c715772f6df8804ff7611ed5c5b2ddc7e8
Kyosuke Takayama <loiseau@gmail.com>
2023-12-13 23:08:36 +0900

    feat: layout search result

--- k6/js/main.js
+++ k6/js/main.js
@@ -28,9 +28,57 @@ function showSearchResult(hospitals) {
 
   const groups = container.selectAll("div").data(hospitals).enter();
 
-  groups.append("div").html((hospital) => {
-    return `<span>${hospital.name}</span><br><span>${hospital.address}</span>`;
-  });
+  groups
+    .append("div")
+    .attr("class", "card")
+    .html((hospital) => {
+      return `
+<div class="meta">
+  <div class="prefecture">【${hospital.prefecture}】</div>
+  <h3 class="title">${hospital.name}</h3>
+  <div class="address">${hospital.address}</div>
+  <div class="phone">${hospital.phone} </div>
+  <div class="url"><a href="${hospital.url}">${hospital.url}</a></div>
+</div>
+
+<div class="information">
+  <div class="flags">
+    <div class="item">人工授精</div>
+    <div class="value">${hospital.flag1 ? "o" : "x"}</div>
+    <div class="item">採卵術</div>
+    <div class="value">${hospital.flag2 ? "o" : "x"}</div>
+    <div class="item">体外受精</div>
+    <div class="value">${hospital.flag3 ? "o" : "x"}</div>
+    <div class="item">顕微授精</div>
+    <div class="value">${hospital.flag4 ? "o" : "x"}</div>
+    <div class="item">新鮮胚移植</div>
+    <div class="value">${hospital.flag5 ? "o" : "x"}</div>
+    <div class="item">凍結・融解胚移植</div>
+    <div class="value">${hospital.flag6 ? "o" : "x"}</div>
+    <div class="item">精巣内精子採取術</div>
+    <div class="value">${hospital.flat7 ? "o" : "x"}</div>
+    <div class="item">顕微鏡下精巣内精子採取術</div>
+    <div class="value">${hospital.flag8 ? "o" : "x"}</div>
+  </div>
+
+  <div class="data">
+    <div class="item">採卵総回数（回）</div>
+    <div class="value">${hospital.frozen_egg}</div>
+    <div class="item">妊娠数（回）</div>
+    <div class="value">${hospital.frozen_preg}</div>
+    <div class="item">生産分娩数（回）</div>
+    <div class="value">${hospital.frozen_birth}</div>
+    <div class="item">移植あたり生産率（%）</div>
+    <div class="value">${hospital.frozen_rate}%</div>
+    <div class="item">&nbsp;</div> <div class="value">&nbsp;</div>
+    <div class="item">&nbsp;</div> <div class="value">&nbsp;</div>
+    <div class="item">&nbsp;</div> <div class="value">&nbsp;</div>
+    <div class="item">&nbsp;</div> <div class="value">&nbsp;</div>
+  </div>
+</div>
+
+`;
+    });
 }
 
 // TSVをデータに変換
