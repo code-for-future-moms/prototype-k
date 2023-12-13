fc140dc4e7eac7346448089436a69fe6149ef7ce
Kyosuke Takayama <loiseau@gmail.com>
2023-12-13 23:07:58 +0900

    chore: add comment

--- k6/js/hospital_store.js
+++ k6/js/hospital_store.js
@@ -38,21 +38,37 @@ class Hospital {
     frozen_birth,
     frozen_rate,
   ) {
+    // 病院名
     this.name = name;
+    // 住所
     this.address = address;
+    // 都道府県
     this.prefecture = prefecture;
+    // 電話番号
     this.phone = phone;
+    // 人工授精
     this.flag1 = flag1;
+    // 採卵術
     this.flag2 = flag2;
+    // 体外受精
     this.flag3 = flag3;
+    // 顕微授精
     this.flag4 = flag4;
+    // 新鮮胚移植
     this.flag5 = flag5;
+    // 凍結・融解胚移植
     this.flag6 = flag6;
+    // 精巣内精子採取術
     this.flat7 = flat7;
+    // 顕微鏡下精巣内精子採取術
     this.flag8 = flag8;
+    // 採卵総回数（回）
     this.frozen_egg = frozen_egg;
+    // 妊娠数（回）
     this.frozen_preg = frozen_preg;
+    // 生産分娩数（回）
     this.frozen_birth = frozen_birth;
+    // 移植あたり生産率（%）
     this.frozen_rate = frozen_rate;
   }
 }
