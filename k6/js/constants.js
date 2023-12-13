1a2ddf102cb6a860daca858a621ed7605c05a2f7
Kyosuke Takayama <loiseau@gmail.com>
2023-12-13 23:07:13 +0900

    feat: update data source

--- k6/js/constants.js
+++ k6/js/constants.js
@@ -1,3 +1,3 @@
 // データ
 const DataSource =
-  "https://raw.githubusercontent.com/code-for-future-moms/prototype-k/f4d71b9279eae5d69b933f98af0594e3b6ae899b/cfa/sample.tsv";
+  "https://raw.githubusercontent.com/code-for-future-moms/fetch-open-data/main/data/cfa-20231213.tsv";
